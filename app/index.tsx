9ab88451b806b8c8c1095251f24435e5d39a7f7f
gr1ntch <odewumimighty@gmail.com>
2025-06-09 01:46:13 +0100

    feat: integrate NativeWind and Tailwind CSS for styling
    - Added metro.config.js to configure NativeWind with Expo.
    - Created nativewind-env.d.ts for TypeScript support.
    - Updated package-lock.json and package.json to include NativeWind and Tailwind CSS dependencies.
    - Added tailwind.config.js for Tailwind CSS configuration.
    - Updated tsconfig.json to include nativewind-env.d.ts for TypeScript type definitions.

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 items-center justify-center bg-blue-100">
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
